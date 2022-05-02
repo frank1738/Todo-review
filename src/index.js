848f939b8d4e1a342a3d9f3c02e40f109abe738b
Frankline Osoro <98453979+frank1738@users.noreply.github.com>
2022-05-02 18:30:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const loadTasks = () => {
   } else {
     const localItems = JSON.parse(localStorage.getItem('tasks'));
     for (let i = 0; i < localItems.length; i += 1) {
-      createBook()
+      const taskElement=createBook()
       const taskDescription = taskElement.querySelector('.description');
       taskDescription.value = localItems[i].description;
       todoContainer.appendChild(taskElement);
